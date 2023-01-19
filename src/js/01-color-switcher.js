974dea7550b46cb4ccc03c1972452c8a2732cb9f
IrynaRozghon <irina.rozgon49@gmail.com>
2023-01-19 16:05:28 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const page = document.querySelector('body');
+let colorful = null;
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+function colorChangeFirst() {
+buttonStart.setAttribute('disabled', '');
+ page.style.backgroundColor = getRandomHexColor();
+}
+
+buttonStart.addEventListener("click", () => {
+  colorChangeFirst();
+  buttonStop.removeAttribute('disabled');
+  colorful = setInterval(() => {
+    page.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+buttonStop.addEventListener("click", () => {
+  clearInterval(colorful);
+  buttonStart.removeAttribute('disabled');
+  buttonStop.setAttribute('disabled', '');
+});
 
