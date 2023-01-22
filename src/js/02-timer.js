aa35d5434969b7da7ceb8ed2a8636528578e06ba
IrynaRozghon <irina.rozgon49@gmail.com>
2023-01-22 17:50:52 +0200

    2 task update 22/01

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,79 +2,70 @@
 import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from "notiflix";
-import "notiflix/dist/notiflix-3.2.6.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-     
-      console.log(selectedDates[0]);
-      currentDifferenceDate(selectedDates[0]);
-  },
+  onClose(selectedDates) {
+    const currentDate = new Date()
+    if (selectedDates[0] < currentDate) {
+      return Notify.failure("Please choose a date in the future");
+    }
+   buttonStart.disabled = false;
+    console.log(selectedDates[0]);
+  }
+   
 };
 // отримуємо доступ до елементів
 const elemInput = document.getElementById("datetime-picker");
-console.log(elemInput);
 const buttonStart = document.querySelector('[data-start]');
-console.log(buttonStart);
-const elemTimer = document.querySelector('.timer');
-console.log(elemTimer);
-const elemField = document.querySelector('.field');
-console.log(elemField);
 const elemDays = document.querySelector('[data-days]');
-console.log(elemDays);
 const elemHours = document.querySelector('[data-hours]');
-console.log(elemHours);
 const elemMinutes = document.querySelector('[data-minutes]');
-console.log(elemMinutes);
 const elemSeconds = document.querySelector('[data-seconds]');
-console.log(elemSeconds);
 
-//встановлюємо початкові значення 
-let timeDifference = 0;
-let timerId = null;
+
+// //встановлюємо початкові значення 
+let timeDifference = null;
 
 
 flatpickr(elemInput, options);
-console.log(flatpickr);
 
-buttonStart.setAttribute('disabled', '');
+buttonStart.disabled = true;
 
-// встановлюємо слухача на кнопку
+// // встановлюємо слухача на кнопку
 buttonStart.addEventListener('click', onbuttonStartClick);
+ 
+function onbuttonStartClick(){
+    const selectedDate = elemInput.value
+    const expectedDate = new Date(selectedDate);
+    timeDifference = setInterval(() => { 
+    const currentDate = new Date()
+    const time = expectedDate - currentDate
+    const timeData = convertMs(time)  
+    const { days, hours, minutes, seconds } = timeData;
+    elemDays.textContent = addLeadingZero(`${days}`)        
+    elemHours .textContent = addLeadingZero(`${hours}`)        
+    elemMinutes.textContent = addLeadingZero(`${minutes}`)        
+    elemSeconds.textContent = addLeadingZero(`${seconds}`)  
+    if (time <= 1000) {
+        clearInterval(timeDifference);
+        return Notify.success('The time has come!');
 
-//запускаю таймер
-function onbuttonStartClick() {
-  timerId = setInterval(timer, 1000);
-  buttonStart.setAttribute('disabled', ''); 
-}
-
-//таймер
-function timer() {
-  buttonStart.setAttribute('disabled', '');
-  elemInput.setAttribute('disabled', true);
-  timeDifference -= 1000;
-
+      }      
+    }, 1000)
+    buttonStart.disabled = true;  
+   
 };
 
-// перевіряю правильність дати
-function currentDifferenceDate(selectedDates) {
-  const currentDate = Date.now();
-
-  if (selectedDates < currentDate) {
-    buttonStart.setAttribute('disabled', '');
-    return Notiflix.Notify.failure('Please choose a date in the future');
-  }
-  timeDifference = selectedDates.getTime() - currentDate;
-  formatDate = convertMs(timeDifference);
-
-  buttonStart.removeAttribute('disabled', '');
+ function addLeadingZero(value){
+    return value.padStart(2, '0')
 };
-
+ 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -92,4 +83,5 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
+  
+} 
